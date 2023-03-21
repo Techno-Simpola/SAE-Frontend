32b18ed81eb150de081aad72cb26c886d4cfc36f
Shibraj Deb <99308927+Debshibraj123@users.noreply.github.com>
2023-03-22 02:08:19 +0530

    Updated arhn.js

--- src/views/Arhn.js
+++ src/views/Arhn.js
@@ -1,11 +1,27 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react'
+import '../assets/css/arhn.css'
+import img from '../../assets/images/200w.gif'
+import About from '../About_Arhn/About'
+import Events from '../Events/Events'
+import Team from '../Events/Team/Team'
+import Gallery from '../Gallery/Gallery'
+import { Member } from '../Member/Member'
+import BackgroundSlider from '../BackgroundSlider/BackgroundSlider'
 
-function Arhn() {
-    return (
-        <>
-        </>
-    );
+const Arhn = () => {
+  return (
+    <>
+    <div className='arhn'>
+     <BackgroundSlider />
+    </div>
+    
+    <About />
+   <Events />
+   <Team />
+   <Gallery />
+   <Member />
+    </>
+  )
 }
 
-export default Arhn;
+export default Arhn
