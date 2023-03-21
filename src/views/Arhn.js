7c54148b0494f82f35996506d8b7cc304e3d1741
Techno-Simpola <rathoreashutosh3006@gmail.com>
2023-03-22 02:57:12 +0530

    adding backgroundSlider

--- src/views/Arhn.js
+++ src/views/Arhn.js
@@ -3,10 +3,9 @@ import '../assets/css/arhn.css';
 import img from '../../assets/images/200w.gif';
 import About from '../components/arhn/arhnAbout';
 import Events from '../components/arhn/arhnEvents';
-import Team from '../components/arhn/arhnTeam';
+import BackgroundSlider from '../components/arhn/BackgroundSlider';
 import Gallery from '../components/arhn/arhnGallery';
 import Member from '../components/arhn/arhnMembers';
-import BackgroundSlider from '../BackgroundSlider/BackgroundSlider'
 
 const Arhn = () => {
   return (
@@ -17,7 +16,6 @@ const Arhn = () => {
     
     <About />
     <Events />
-    <Team />
     <Gallery />
     <Member />
     </>
