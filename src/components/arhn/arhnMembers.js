a6dc45c14dad619303a02800952a87b3a14279e8
Shibraj Deb <99308927+Debshibraj123@users.noreply.github.com>
2023-03-22 02:50:51 +0530

    Updated arhnMembers.js

---
+++ src/components/arhn/arhnMembers.js
@@ -0,0 +1,179 @@
+import React from 'react'
+import 'import "../assets/css/arhnCSS/arhnMembers.css"'
+
+export const Member = () => {
+  return (
+    <>
+       
+<div class="container pb-4">
+    <div class="row blog">
+        <h1 class="center mx-auto text-center py-4">Our Team Members</h1>
+       
+	   <div class="col-md-12">
+            <div id="blogCarousel" class="carousel slide" data-ride="carousel">
+
+                <ol class="invisible carousel-indicators">
+                    <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#blogCarousel" data-slide-to="1"></li>
+                </ol>
+
+               
+                <div class="carousel-inner">
+
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Dana Robins</h3>
+                                        <span class="post">Marketing Consultant</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">John Doe</h3>
+                                        <span class="post">Marketing Consultant</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Markus Baas</h3>
+                                        <span class="post">Financial Expert</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Sophia Lee</h3>
+                                        <span class="post">Customer Support</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                      
+                    </div>
+                    
+
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Ted Robbins</h3>
+                                        <span class="post">Law Expert</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Noel Flantier</h3>
+                                        <span class="post">Marketing Consultant</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Ernesto Appia</h3>
+                                        <span class="post">Team Leader</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="our-team">
+                                    <div class="pic">
+                                        <img src="https://i.ibb.co/L8Pj1mg/o6EuTCT6.jpg"/>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3 class="title">Rosita Jimenez</h3>
+                                        <span class="post">Marketing Consultant</span>
+                                    </div>
+                                    <ul class="social">
+                                        <li>
+                                            <a href="#" class="fa fa-envelope"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                   
+
+                </div>
+               
+            </div>
+          
+
+        </div>
+    </div>
+</div>
+    </>
+  )
+}
