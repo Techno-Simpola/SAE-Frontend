3a70418447a1512f955f7bef02bb817f988ea64c
Shibraj Deb <99308927+Debshibraj123@users.noreply.github.com>
2023-03-22 02:47:17 +0530

    Updated arhnEvents.js

---
+++ src/components/arhn/arhnEvents.js
@@ -0,0 +1,157 @@
+import React from "react";
+import img1 from "../../assets/images/1.png";
+import "import "../assets/css/arhnCSS/arhnEvents.css"";
+
+const Events = () => {
+  return (
+    <div className="container">
+
+      <h2 className="new">Our Events And Workshop In Aarohan</h2>
+   <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://arhn.co.in/media/event_images/Mazemerize_fina-02_1_1.png" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://arhn.co.in/media/event_images/Transmission-2023_Final_1.png" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>   
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://arhn.co.in/media/event_images/Bidwit.jpeg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>     
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://arhn.co.in/media/event_images/Deathnote.jpeg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://arhn.co.in/media/event_images/WhatsApp_Image_2023-03-07_at_15.21.03.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://arhn.co.in/media/workshop_images/WhatsApp_Image_2023-03-07_at_15.39.48.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        {/* <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>  
+
+        <div className="card-container">
+      <a href="/" className="hero-image-container">
+        <img className="hero-image" src="https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg" alt="Spinning glass cube"/>
+      </a>
+      <main className="main-content">
+        <h1 className="hdng"><a href="#">Equilibrium #3429</a></h1>
+        <p className="para">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex-row">
+        
+        </div>
+        </main>
+        </div>   */}
+    </div>
+  );
+};
+
+export default Events;
