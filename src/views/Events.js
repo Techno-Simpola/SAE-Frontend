60b13b67e20cee09ac68d51d250bac60088206e8
Himanshu Hembram <98567174+himanshu456ia@users.noreply.github.com>
2023-03-22 17:09:37 +0530

    updated events.js

--- src/views/Events.js
+++ src/views/Events.js
@@ -3,11 +3,70 @@ import classNames from 'classnames';
 import "../assets/css/events.css";
 // import "../components/elements/eventsCarousel.js";
 
-function Events() {
+const Events = () => {
   return (
-    <>  
-    </>
-  );
+<>
+
+
+ <div class="event-wrapper">
+ <div class="event-hero">
+  <h1 className='event-h1 stroke-shadow'>
+    <a href="#">OUR EVENTS</a>
+    </h1>
+</div> 
+</div>
+<div className='heading1'>
+<h1 >Drag and Drift</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus doloremque, illum voluptatibus aut voluptate similique dolore reprehenderit. Nulla possimus quibusdam quis, quisquam molestiae perferendis dignissimos deleniti ullam corporis ipsum quaerat.</p>
+</div>
+
+{/* Drag and Drift contents */}
+
+<section class="DandD-section">
+  <div class="DandDgrid">
+    <div class="item">
+    </div>
+    <div class="item item--large">
+    </div>
+    <div class="item item--medium">
+    </div>
+    <div class="item item--large">
+    </div>
+    <div class="item item--medium">
+    </div>
+    <div class="item">
+    </div>
+  </div>
+</section>
+
+
+<div className='heading1'>
+<h1 >Self Driving Vehicle</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus doloremque, illum voluptatibus aut voluptate similique dolore reprehenderit. Nulla possimus quibusdam quis, quisquam molestiae perferendis dignissimos deleniti ullam corporis ipsum quaerat.</p>
+</div>
+
+{/* Self Driving Vehicle contents */}
+<section class="sdvSection">
+
+<div class="sdv-grid">
+
+<div class="sdv-item item--large">
+  </div>
+  <div class="sdv-item item--medium">
+  </div>
+
+  <div class="sdv-item">
+  </div>
+
+  <div class="sdv-item item--medium">
+  </div>
+ 
+  <div class="sdv-item">
+  </div>
+</div>
+</section>
+</>
+  )
 }
 
-export default Events;
+export default Events
