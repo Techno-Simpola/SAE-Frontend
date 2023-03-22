a2c788f346c25d1aa789ce527d8d5305a574ba9f
Techno-Simpola <rathoreashutosh3006@gmail.com>
2023-03-22 19:37:16 +0530

    adding faculty and offB structure

---
+++ src/components/sections/Faculty.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Faculty() {
+    return (
+        <div>
+        <h1>Faculty</h1>
+        </div>
+    );
+}
+
+export default Faculty;
---
+++ src/components/sections/OffB.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function OffB() {
+    return (
+        <div>
+        <h1>Faculty</h1>
+        </div>
+    );
+}
+
+export default OffB;
