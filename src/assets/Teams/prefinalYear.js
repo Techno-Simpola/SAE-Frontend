6f9dbf159b43ae47edb93624357ed6004431fa0b
hritam123 <112815523+hritam123@users.noreply.github.com>
2023-03-22 18:21:29 +0530

    created prefinalYear.js

---
+++ src/assets/Teams/prefinalYear.js
@@ -0,0 +1,33 @@
+const prefinalYear = [ {
+    "url":"https://i.ibb.co/x1Gth6s/team-1.jpg",
+    "name":"Sujal Gupta",
+    "position":"Senior coordinator",
+    "instaLink":"",
+    "linkedinLink":"",
+},
+{
+    "url":"https://i.ibb.co/VNnNB0P/team-2.jpg",
+    "name":"Ishan Gupta",
+    "position":"Senior coordinator",
+    "instaLink":"",
+    "linkedinLink":"",
+},
+{
+    "url":"https://i.ibb.co/2MP418L/team-3.jpg",
+    "name":"Asutosh Kumar Singh Rathore",
+    "position":"Senior coordinator",
+    "instaLink":"",
+    "linkedinLink":"",
+},
+{
+    "url":"https://i.ibb.co/jGcmRHP/team-4.jpg" ,
+    "name":"Vaibhab Ojha",
+    "position":"Senior coordinator",
+    "instaLink":"",
+    "linkedinLink":"",
+}
+
+
+]
+
+export default prefinalYear;
